21c2dd243b07b9572bbe1a0605affa7b1524142e
donny2131 <55747762+donny2131@users.noreply.github.com>
2020-01-25 16:25:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "#";
+var prefix = ".";
 client.on('message', message => {
 if(!message.content.startsWith(prefix)) return;
 let command = message.content.split(" ")[0];
@@ -14,39 +14,38 @@ let broadcastt = new Discord.RichEmbed().setColor('#36393e')
 .setDescription(`**Please type the number of your chose**`)
 .setFooter('you can add to the message [user] = mention the user')
 message.channel.send(broadcastt).then(msg => {
-message.channel.awaitMessages(filter, {max: 1,time: 10000,errors: ['time']})
+message.channel.awaitMessages(filter, {max: 1,time: 5000,errors: ['time']})
 .then(collected => {if(collected.first().content === '1') {msg.delete(),message.channel.send(`**☑ Broadcast begin send....**`).then(m => {
 message.guild.members.map(member => {setTimeout(() => {member.send(args.replace('[user]',member).replace('[icon]',`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png?size=1024`)).then(() => {}).catch((err) => {});},);});})}
-if(collected.first().content === '2') {msg.delete(),message.channel.bulkDelete(1),message.channel.send(`**☑ Broadcast begin send....**`);
+if(collected.first().content === '2') {msg.delete(),message.channel.bulkDelete(1),message.channel.send(`**☑ Broadcast begin send.**`);
 message.guild.members.filter(m => m.presence.status === 'online').forEach(m => {m.send(args.replace('[user]', m))})
 message.guild.members.filter(m => m.presence.status === 'dnd').forEach(m => {m.send(args.replace('[user]', m)) })
 return message.guild.members.filter(m => m.presence.status === 'idle').forEach(m => {m.send(args.replace('[user]', m)) })}
 if(collected.first().content === '0') {msg.delete(),message.channel.bulkDelete(1);return message.channel.send(`**Broadcast Has Been Canseled**`);}
 if(collected.first().content === '3') {msg.delete();message.channel.bulkDelete(1);
 message.channel.send('**Please Type the role name or id.**');
-message.channel.awaitMessages(filter, {max: 1,time: 20000,errors: ['time']}).then(t => {
+message.channel.awaitMessages(filter, {max: 1,time: 40000,errors: ['time']}).then(t => {
 let R = t.first().content;
 let role = message.guild.roles.find('name',R) || message.guild.roles.get(R);
 if(!role) return message.channel.send('**😕 I Can\'t find this role please try again**'),msg.delete();
 message.channel.bulkDelete(2);
 if(role.members.size < 1) return message.channel.send('**there is no one have this role **😕');;
-let XYZ = new Discord.RichEmbed().setTitle('**:ballot_box_with_check: Broadcast begin send....**').setDescription(`**For the role: ${role}**`).setColor(role.color)
+let XYZ = new Discord.RichEmbed().setTitle('**:ballot_box_with_check: Broadcast begin send.**').setDescription(`**For the role: ${role}**`).setColor(role.color)
 message.channel.send(XYZ)
 message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {setTimeout(() => {n.send(args.replace('[user]',n)).catch((err) => {});});});}).catch(err =>{});}
 if(!collected.first().content.includes(['1','2','3','4','0'])) {msg.edit('Canceled.')}
 if(collected.first().content === '4') { msg.delete();
 message.channel.send('**✅ Please Type the photo link now**,Type "cansel" to cansel.').then(msgg =>{
-message.channel.awaitMessages(filter, {max: 1,time: 10000,errors: ['time']}).then(XX => {
+message.channel.awaitMessages(filter, {max: 1,time: 50000,errors: ['time']}).then(XX => {
 let photo = XX.first().content; if(photo == 'cansel') {message.channel.bulkDelete(2); return message.channel.send('**Broadcast Has Been Canseled**')}
 let embed = new Discord.RichEmbed().setImage(photo).setTitle(`**are you sure you want to send this? \`[y,n]\`**`).setColor('#36393e')
 message.channel.send(embed).catch(e =>{return message.channel.send('**The Photo link is wrong :x:**')});
 let filter = m => m.author.id == message.author.id
-message.channel.awaitMessages(filter, {max: 1,time: 10000,errors: ['time']}).then(XD => {if(XD.first().content === 'y') {
+message.channel.awaitMessages(filter, {max: 1,time: 90000,errors: ['time']}).then(XD => {if(XD.first().content === 'y') {
 let bc = new Discord.RichEmbed().setTitle(`${args}`).setImage(photo).setFooter(message.guild.name,`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png?size=1024`)
 message.channel.bulkDelete(2);msgg.delete();message.channel.send('**☑ Broadcast begin send....**');message.guild.members.map(member => {setTimeout(() => {member.send(bc)}
 )})}if(XD.first().content == 'n') {message.channel.bulkDelete(2);message.channel.send('**Broadcast Has Been Canseled**')}
 })}).catch(myst =>{msgg.edit('Timed out.');})})
 }if(collected.first().content === '5'){} // لو تبي تضيف شي خامس :]
 }).catch(mys =>{msg.edit('Timed out to chose.')})})}});
-
 client.login(process.env.BOT_TOKEN);
